9b50493769b8b9b9238116391dc1c3be419b5f46
Ridercogswell <ridercogswell@gmail.com>
2022-03-30 14:58:03 -0700

    setup connection to db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const hbs = exphbs.create({});
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('.config/connection');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,4 +23,4 @@ app.use(routes);
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-})
+});
