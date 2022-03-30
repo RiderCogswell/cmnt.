32776acef1996a0c5146df5f02713434c7e80bfb
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-30 13:00:32 -0700

    created index in models with imports and associations

---
+++ models/Index.js
@@ -0,0 +1,69 @@
+//model imports
+const Topic = require('./Topic');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
+
+//associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  Topic.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.belongsToMany(Topic, {
+    through: Vote,
+    as: 'voted_posts',
+  
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Topic.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'topic_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Vote.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Vote.belongsTo(Topic, {
+    foreignKey: 'topic_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Topic.hasMany(Vote, {
+    foreignKey: 'topic_id'
+  });
+  
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Comment.belongsTo(Topic, {
+    foreignKey: 'topic_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Topic.hasMany(Comment, {
+    foreignKey: 'topic_id'
+  });
+
+module.exports = { Topic, User, Vote, Comment };
