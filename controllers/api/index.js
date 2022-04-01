8e22438d8dd77952819b5565834d1146f84eccc2
Ridercogswell <ridercogswell@gmail.com>
2022-03-31 19:54:51 -0700

    fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./user-routes');
 const topicRoutes = require('./topic-routes');
 const commentRoutes = require('./comment-routes');
 
-router.use('./users', userRoutes);
-router.use('./topics', topicRoutes);
-router.use('./comments', commentRoutes);
+router.use('/users', userRoutes);
+router.use('/topics', topicRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
