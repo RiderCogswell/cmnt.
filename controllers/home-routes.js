bc91badfc3e00531fabb283488c9120e64eba0de
Ridercogswell <ridercogswell@gmail.com>
2022-04-05 18:42:03 -0700

    minor edit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ router.get('/', withAuth, (req, res) => {
     .then(dbPostData => {
       const topics = dbPostData.map(topic => topic.get({ plain: true }));
 
-      let max = topics.length-1;
+      let max = topics.length - 1;
       let randomNumber = Math.floor(Math.random() * (max - 1 + 1)) + 1
       
       const topic = topics[randomNumber];
@@ -65,62 +65,62 @@ router.get('/', withAuth, (req, res) => {
 });
   
   // get single post
-  router.get('/topic/:id', (req, res) => {
-    Topic.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE topic.id = vote.topic_id)'), 'likes']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'topic_id', 'user_id'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+router.get('/topic/:id', (req, res) => {
+  Topic.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE topic.id = vote.topic_id)'), 'likes']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'topic_id', 'user_id'],
+        include: {
           model: User,
           attributes: ['username']
         }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-  
-        const topic = dbPostData.get({ plain: true });
-  
-        res.render('single-topic', {
-          topic,
-          loggedIn: req.session.loggedIn
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
-  router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
-  router.get('/signup', (req, res) => {
-    res.render('signup');
-  });
+      const topic = dbPostData.get({ plain: true });
+
+      res.render('single-topic', {
+        topic,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
 
 module.exports = router;
