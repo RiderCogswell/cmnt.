62553db3460114c95799c45d69835fbe73cff14c
Ridercogswell <ridercogswell@gmail.com>
2022-04-06 21:07:29 -0700

    fixed login

--- public/js/login.js
+++ public/js/login.js
@@ -7,6 +7,7 @@ const loginFormHandler = async (event) => {
   if (username && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
+      mode: 'cors',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' }
     });
